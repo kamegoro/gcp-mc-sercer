18549563c8e9a4c77f2706315d2ed4dff375d7f1
kame <tentyo0620@icloud.com>
2023-03-23 20:53:37 +0900

    settings gcp machine server

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,3 +2,23 @@ variable "project" {
   type    = string
   default = "smaple"
 }
+
+variable "region" {
+  type    = string
+  default = "asia-northest1"
+}
+
+variable "zone" {
+  type    = string
+  default = "asia-northest1-a"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "n1-standard-1"
+}
+
+variable "network_tags" {
+  type    = list(string)
+  default = ["minecraft"]
+}
