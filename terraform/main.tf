5fa188c23732e4feebb2b4161908fb6df5728b07
kame <tentyo0620@icloud.com>
2023-03-25 14:42:29 +0900

    Delete env file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("${path.module}/mc-server-terraform_serviceaccount_credential.json")
+  credentials = file("./../mc-server-terraform_serviceaccount_credential.json")
   project = "mc-server-381513"
   region  = var.region
 }
